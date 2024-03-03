028834bd9b5f2987f22495528aefd16177616d54
Marcos Crespo <marcoscrespo.diaz@gmail.com>
2024-03-03 16:44:37 +0100

    git ignore and some work

--- Data Tidying and Reporting/Task_2/Visual_Mnist/app.R
+++ Data Tidying and Reporting/Task_2/Visual_Mnist/app.R
@@ -7,45 +7,32 @@
 #    http://shiny.rstudio.com/
 #
 
+###############################################################################
+###############################################################################
+
+#Notes:
+
+# - Make an action button for predicting?
+
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Visulizaton and classification of Mnist images"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "number", label = "Digit selector",
+                  min = 0, max = 9, value=7, step=1, ticks = TRUE, round=TRUE),
+    ),
+    mainPanel(
+      imageOutput(outputId = "digit.png")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+server <-function(input, output) {}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
