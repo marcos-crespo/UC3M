70cbda497c548008478f8689eb9a910af8f78ade
Marcos Crespo <marcoscrespo.diaz@gmail.com>
2024-03-26 23:45:16 +0100

    update gitignore

--- High Performance Computing/pruebas_cpp2.cpp
+++ High Performance Computing/pruebas_cpp2.cpp
@@ -1,30 +1,79 @@
 #include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// - Result of the matrix chain multiplication
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// Function to perform matrix multiplication based on a given order
-// Inputs:
-//   matrices: List of matrices
-//   order: Vector defining the order of matrix multiplication
-// Returns:
-//   Result of the matrix multiplication
+bool checkMatrixDimensions(const Rcpp::NumericMatrix& A, const Rcpp::NumericMatrix& B) {
+  int nrowA = A.nrow();
+  int ncolA = A.ncol();
+  int nrowB = B.nrow();
+  int ncolB = B.ncol();
+  
+  // Check if dimensions are suitable for multiplication
+  return (ncolA == nrowB);
+}
+bool anyDuplicates(IntegerVector vec) {
+  std::unordered_set<double> seen;
+  for (int i = 0; i < vec.size(); ++i) {
+    if (seen.find(vec[i]) != seen.end()) {
+      return true;
+    } else {
+      seen.insert(vec[i]);
+    }
+  }
+  return false;
+}
+
+bool checkDimensions(IntegerVector vec, int dims) {
+  for (int i = 0; i < vec.size(); ++i) {
+    if (vec[i] > dims) {
+      return false;
+    }
+  }
+  return true;
+}
 // [[Rcpp::export]]
-Rcpp::NumericMatrix multiplyMatricesInOrder(Rcpp::List matrices, Rcpp::IntegerVector order) {
+arma::mat matrixChainMultiplication(List matrices, IntegerVector order) {
   // Check if the order vector is valid
   if (order.size() != matrices.size() - 1) {
     Rcpp::stop("Invalid order vector size");
   }
-  
-  // Initialize result matrix
-  arma::mat result = arma::eye<arma::mat>(matrices[0].rows(), matrices[order[0]].cols());
-  
-  // Perform matrix multiplication based on the given order
-  for (int i = 0; i < order.size(); ++i) {
-    arma::mat currentMat = arma::mat(Rcpp::as<arma::mat>(matrices[order[i]]));
-    result *= currentMat;
+  for(int i=0; i<matrices.length()-1;i++){
+    if (!checkMatrixDimensions(matrices[i], matrices[i+1])){
+      Rcpp::stop("Invalid matrices sizes. Check matrix dimensions");
+    }
+  }
+  if (anyDuplicates(order)){
+    Rcpp::stop("Invalid order vector. Check for duplicates");
+  }
+  if (!checkDimensions(order,matrices.size()-2)){
+    Rcpp::stop("Invalid order number. Number out of bounds");
   }
   
-  // Convert Armadillo matrix back to Rcpp::NumericMatrix
-  Rcpp::NumericMatrix resultR = Rcpp::wrap(result);
+  LogicalVector indicator(order.size(), false);
+  IntegerVector index(order.size());
+  List steps(order.size());
+  int cont =1;
   
-  return resultR;
+  for (int i = 0; i < order.size(); i++) {
+    int curr_order = order[i];
+    arma::mat l, r;
+    if (!indicator[curr_order -1]){
+      arma::mat l = as<arma::mat>(matrices[curr_order]);
+    } else {
+      arma::mat l = as<arma::mat> (steps[index[curr_order-1]]);
+    }
+    if (!indicator[curr_order +1]){
+      arma::mat r = as<arma::mat>(matrices[curr_order+1]);
+    } else {
+      arma::mat r= as<arma::mat> (steps[index[curr_order+1]]);
+    }
+    steps[i] = l*r;
+    index[curr_order] = cont;
+    cont++;
+  }
+  
+  return steps[order.size()-1];
 }
+
